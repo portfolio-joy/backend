05d22c44019723100c656c87e80c63eb1fc4f4b5
Chirag Panjwani <chirag.panjwani@genesistechnologies.in>
2024-08-23 20:13:48 +0530

    created update and delete routes at controller

--- src/main/java/com/joy/portfolio/controller/UserController.java
+++ src/main/java/com/joy/portfolio/controller/UserController.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -64,5 +66,18 @@ public class UserController {
 	public ResponseEntity<Skill> addSkill(@RequestBody @Valid SkillDto skillDto)
 	{
 		return ResponseEntity.status(HttpStatus.CREATED).body(userService.addSkill(skillDto));
+	}
+	
+	@PutMapping(value="/skill")
+	public ResponseEntity<Skill> updateSkill(@RequestParam String id, @RequestBody @Valid SkillDto skillDto)
+	{
+		return ResponseEntity.status(HttpStatus.CREATED).body(userService.updateSkill(id,skillDto));
+	}
+	
+	@DeleteMapping(value="/skill")
+	public ResponseEntity<Skill> removeSkill(@RequestParam String id)
+	{
+		userService.removeSkill(id);
+		return ResponseEntity.ok(null);
 	}
 }
