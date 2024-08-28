342773421345f8c1629900c5c75be91d61e0f94a
Chirag Panjwani <chirag.panjwani@genesistechnologies.in>
2024-08-28 19:25:53 +0530

    created backend apis to add, update and delete the project

---
+++ src/main/java/com/joy/portfolio/service/ProjectService.java
@@ -0,0 +1,55 @@
+package com.joy.portfolio.service;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.joy.portfolio.dto.ProjectDto;
+import com.joy.portfolio.entity.Image;
+import com.joy.portfolio.entity.Project;
+import com.joy.portfolio.mapper.ProjectMapper;
+import com.joy.portfolio.repository.ImageRepository;
+import com.joy.portfolio.repository.ProjectRepository;
+
+@Service
+public class ProjectService {
+
+	@Autowired
+	ProjectRepository projectRepository;
+	
+	@Autowired
+	ImageRepository imageRepository;
+	
+	@Autowired
+	ProjectMapper projectMapper;
+	
+	public Project addProject(ProjectDto projectDto) throws IOException {
+		MultipartFile image = projectDto.getImage();
+		Image projectImage = new Image(image.getOriginalFilename(), image.getContentType(), image.getBytes());
+		projectImage = imageRepository.save(projectImage);
+		Project project = projectMapper.mapDtoToProject(projectDto);
+		project.setImage(projectImage);
+		return projectRepository.save(project);
+	}
+	
+	public Project updateProject(String id, ProjectDto projectDto) throws IOException {
+		Project project = projectRepository.findById(id).orElse(null);
+		String oldImageId = project.getImage().getId();
+		MultipartFile image = projectDto.getImage();
+		Image projectImage = new Image(image.getOriginalFilename(), image.getContentType(), image.getBytes()); 
+		projectImage = imageRepository.save(projectImage);
+		project = projectMapper.mapDtoToProject(projectDto);
+		project.setId(id);
+		project.setImage(projectImage);
+		project = projectRepository.save(project);
+		imageRepository.deleteById(oldImageId);
+		return project;
+	}
+	
+	public void removeProject(String id) {
+		Project project = projectRepository.findById(id).orElse(null);
+		imageRepository.delete(project.getImage());
+	}
+}
